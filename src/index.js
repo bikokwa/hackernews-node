83ab7039089f3f93ff5db5e883538e8924d09fdd
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-12-20 06:48:29 +0300

    A Simple Query

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
   },
 };
 
